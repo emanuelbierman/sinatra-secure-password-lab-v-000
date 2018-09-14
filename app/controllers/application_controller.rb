a7ab4c608d9b75941daffde2d26a48c1967a8f02
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-14 17:08:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     user = User.new(:username => params[:username], :password => params[:password])
-    if params[:username].blank? || params[:password].blank?
+    if params[:username].blank? || params[:password].blank? || !user.save
       redirect '/failure'
     elsif user.save
       session[:user_id] = params[:password]
