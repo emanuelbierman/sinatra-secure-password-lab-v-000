cb926cf49e5da3e756f732791666be3ec164b0cc
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-14 16:47:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.find(params)
+    user = User.new(params)
     if user
       redirect '/account'
     else
